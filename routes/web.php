06a51c9114af3714eb3f637377608d53f03f5769
Md Ariful Islam <arifulatwork@gmail.com>
2023-09-03 23:20:57 +0600

    Laravel route create for signUp and SignIn

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signin', [CustomAuthController::class, 'signIn']);
+
+Route::get('/signup',[CustomAuthController::class, 'signUp']);

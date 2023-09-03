2759f633c51737550c33f34e5ee760ab6f25cd64
Md Ariful Islam <arifulatwork@gmail.com>
2023-09-03 22:37:52 +0600

    Create controller and methods

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomAuthController extends Controller
+{
+    // Sign In
+
+    public function signIn(){
+
+        return "Hello World Piti";
+
+
+    }
+
+    // Sign Up
+
+    public function signUp(){
+
+        return "Hello World Kiti";
+
+    }
+}
